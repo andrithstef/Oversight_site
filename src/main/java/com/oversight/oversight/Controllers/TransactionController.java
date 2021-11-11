efd6b3db2aa9ed63ee0cc0ed143f4d96bc0d7c7f
krisak11 <kristoferisak@gmail.com>
2021-11-11 14:18:08 +0000

    bætti við color-scheme css og js skrám

--- src/main/java/com/oversight/oversight/Controllers/TransactionController.java
+++ src/main/java/com/oversight/oversight/Controllers/TransactionController.java
@@ -37,7 +37,7 @@ public class TransactionController {
     }
 
     @RequestMapping(value="/addTransaction", method = RequestMethod.GET)
-    public String addTransactionGET(Transaction transaction){
+    public String addTransactionGET(Transaction transaction, User user){
         return "newTransaction";
     }
 
