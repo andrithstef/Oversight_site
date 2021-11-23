4f90365ec782051ca5f0a07c2f5419c6c8cfffb7
Biggiboggo <bis37@hi.is>
2021-11-23 19:11:28 +0000

    Spending plan komið

--- src/main/java/com/oversight/oversight/Persistence/Entities/SpendingPlan.java
+++ src/main/java/com/oversight/oversight/Persistence/Entities/SpendingPlan.java
@@ -1,7 +1,6 @@
 package com.oversight.oversight.Persistence.Entities;
 
 import javax.persistence.*;
-import java.util.ArrayList;
 
 @Entity
 @Table(name="spendingplan")
@@ -10,24 +9,43 @@ public class SpendingPlan {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long ID;
 
-    private float food;
-    private float car;
-    private float booze;
-    private float electronics;
-    
+    private int cars_transportation;
+    private int children;
+    private int education;
+    private int fines_fees;
+    private int food;
+    private int health_beauty;
+    private int home;
+    private int insurance;
+    private int investments_savings;
+    private int leisure;
+    private int shopping_services;
+    private int uncategorized;
+    private int vacation_travel;
+
     @OneToOne
     private User user;
 
     public SpendingPlan() {
     }
 
-    public SpendingPlan(User user, float food, float car, float booze, float electronics) {
-        this.user = user;
+    public SpendingPlan(int cars_transportation, int children, int education, int fines_fees, int food,
+                        int health_beauty, int home, int insurance, int investments_savings,
+                        int leisure, int shopping_services, int uncategorized, int vacation_travel, User user) {
+        this.cars_transportation = cars_transportation;
+        this.children = children;
+        this.education = education;
+        this.fines_fees = fines_fees;
         this.food = food;
-        this.car = car;
-        this.booze = booze;
-        this.electronics = electronics;
-
+        this.health_beauty = health_beauty;
+        this.home = home;
+        this.insurance = insurance;
+        this.investments_savings = investments_savings;
+        this.leisure = leisure;
+        this.shopping_services = shopping_services;
+        this.uncategorized = uncategorized;
+        this.vacation_travel = vacation_travel;
+        this.user = user;
     }
 
     public long getID() {
@@ -38,43 +56,115 @@ public class SpendingPlan {
         this.ID = ID;
     }
 
+    public int getCars_transportation() {
+        return cars_transportation;
+    }
+
+    public void setCars_transportation(int cars_transportation) {
+        this.cars_transportation = cars_transportation;
+    }
+
+    public int getChildren() {
+        return children;
+    }
+
+    public void setChildren(int children) {
+        this.children = children;
+    }
+
+    public int getEducation() {
+        return education;
+    }
+
+    public void setEducation(int education) {
+        this.education = education;
+    }
+
+    public int getFines_fees() {
+        return fines_fees;
+    }
+
+    public void setFines_fees(int fines_fees) {
+        this.fines_fees = fines_fees;
+    }
+
+    public int getFood() {
+        return food;
+    }
+
+    public void setFood(int food) {
+        this.food = food;
+    }
+
+    public int getHealth_beauty() {
+        return health_beauty;
+    }
+
+    public void setHealth_beauty(int health_beauty) {
+        this.health_beauty = health_beauty;
+    }
+
+    public int getHome() {
+        return home;
+    }
+
+    public void setHome(int home) {
+        this.home = home;
+    }
+
+    public int getInsurance() {
+        return insurance;
+    }
+
+    public void setInsurance(int insurance) {
+        this.insurance = insurance;
+    }
+
+    public int getInvestments_savings() {
+        return investments_savings;
+    }
+
+    public void setInvestments_savings(int investments_savings) {
+        this.investments_savings = investments_savings;
+    }
+
+    public int getLeisure() {
+        return leisure;
+    }
+
+    public void setLeisure(int leisure) {
+        this.leisure = leisure;
+    }
+
+    public int getShopping_services() {
+        return shopping_services;
+    }
+
+    public void setShopping_services(int shopping_services) {
+        this.shopping_services = shopping_services;
+    }
+
+    public int getUncategorized() {
+        return uncategorized;
+    }
+
+    public void setUncategorized(int uncategorized) {
+        this.uncategorized = uncategorized;
+    }
+
+    public int getVacation_travel() {
+        return vacation_travel;
+    }
+
+    public void setVacation_travel(int vacation_travel) {
+        this.vacation_travel = vacation_travel;
+    }
+
     public User getUser() {
         return user;
     }
 
     public void setUser(User user) {
         this.user = user;
-    }
-
-    public float getFood() {
-        return food;
-    }
-
-    public void setFood(float food) {
-        this.food = food;
-    }
-
-    public float getCar() {
-        return car;
-    }
-
-    public void setCar(float car) {
-        this.car = car;
-    }
-
-    public float getBooze() {
-        return booze;
-    }
-
-    public void setBooze(float booze) {
-        this.booze = booze;
-    }
-
-    public float getElectronics() {
-        return electronics;
-    }
-
-    public void setElectronics(float electronics) {
-        this.electronics = electronics;
     }
 }
--- src/main/java/com/oversight/oversight/Services/Implementations/SpendingPlanServiceImplementation.java
+++ src/main/java/com/oversight/oversight/Services/Implementations/SpendingPlanServiceImplementation.java
@@ -7,7 +7,7 @@ import com.oversight.oversight.Services.SpendingPlanService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.util.*;
 
 @Service
 public class SpendingPlanServiceImplementation implements SpendingPlanService {
@@ -41,6 +41,46 @@ public class SpendingPlanServiceImplementation implements SpendingPlanService {
     @Override
     public SpendingPlan findByUser(User user) {
         return spendingPlanRepository.findByUser(user);
+    }
+
+    @Override
+    public ArrayList<ArrayList<Object>> getPieChartData(User user) {
+
+        //Create dictionary
+        HashMap<String, Integer> map = new HashMap<String, Integer>();
+
+        //Create empty list
+        ArrayList<ArrayList<Object>> chartData = new ArrayList<ArrayList<Object>>();
+
+        //get all transactions
+        SpendingPlan spendingPlan = spendingPlanRepository.findByUser(user);
+
+        //Add each transaction to the empty list as a pair of category and amount
+        map.put("Cars & Transportation", spendingPlan.getCars_transportation());
+        map.put("Children", spendingPlan.getChildren());
+        map.put("Education", spendingPlan.getEducation());
+        map.put("Fines & Fees", spendingPlan.getFines_fees());
+        map.put("Food", spendingPlan.getFood());
+        map.put("Health & Beauty", spendingPlan.getHealth_beauty());
+        map.put("Home", spendingPlan.getHome());
+        map.put("Insurance", spendingPlan.getInsurance());
+        map.put("Investments & Savings", spendingPlan.getInvestments_savings());
+        map.put("Leisure", spendingPlan.getLeisure());
+        map.put("Shopping & Services", spendingPlan.getShopping_services());
+        map.put("Uncategorized", spendingPlan.getUncategorized());
+        map.put("Vacation & Travel", spendingPlan.getVacation_travel());
+
+        Iterator<Map.Entry<String, Integer>> entrySet = map.entrySet().iterator();
+
+        while(entrySet.hasNext()){
+            Map.Entry<String, Integer> entry = entrySet.next();
+            ArrayList<Object> temp = new ArrayList<Object>();
+            temp.add(entry.getKey());
+            temp.add(entry.getValue());
+            chartData.add(temp);
+        }
+
+        return chartData;
     }
     /*
     @Override
